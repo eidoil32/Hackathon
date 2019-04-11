b6e79604962b528125864682cf19ff729109b605
Haim Zisman <haimzismann@gmail.com>
2019-04-11 09:59:51 +0300

    test

--- app/src/main/java/com/idohayun/bracelethackathon/MainActivity.java
+++ app/src/main/java/com/idohayun/bracelethackathon/MainActivity.java
@@ -4,10 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+ //my name is haim
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState); //Haim
         setContentView(R.layout.activity_main);
     }
 }

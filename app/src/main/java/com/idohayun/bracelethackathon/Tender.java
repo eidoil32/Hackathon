d08c00d93fabcb0d8754eca3645c3fc62d7f95a7
tim89 <tim89909@gmail.com>
2019-04-11 10:38:08 +0300

    objects for users

---
+++ app/src/main/java/com/idohayun/bracelethackathon/Tender.java
@@ -0,0 +1,12 @@
+package com.idohayun.bracelethackathon;
+
+public class Tender {
+    int id;
+    String name;
+    String password;
+    enum permissions
+    {
+        Doctor,Paramedic
+    }
+
+}
